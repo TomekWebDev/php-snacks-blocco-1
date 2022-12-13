117ec224600a2505991d53125f67f742b9c8079c
TomekWebDev <tommaso.mangiagalli@gmail.com>
2022-12-13 15:15:38 +0100

    3

--- Snack3/index.php
+++ Snack3/index.php
@@ -1,3 +1,54 @@
+<!-- Snack 3
+Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 
+e come valore un array di post associati a quella data. 
+Stampare ogni data con i relativi post. -->
+
+
+<!--  -->
+
+<?php
+
+$posts = [
+
+  '10/01/2019' => [
+    [
+      'title' => 'Post 1',
+      'author' => 'Michele Papagni',
+      'text' => 'Testo post 1'
+    ],
+    [
+      'title' => 'Post 2',
+      'author' => 'Michele Papagni',
+      'text' => 'Testo post 2'
+    ],
+  ],
+  '11/01/2019' => [
+    [
+      'title' => 'Post 3',
+      'author' => 'Michele Papagni',
+      'text' => 'Testo post 3'
+    ]
+  ],
+  '12/01/2019' => [
+    [
+      'title' => 'Post 4',
+      'author' => 'Michele Papagni',
+      'text' => 'Testo post 4'
+    ],
+    [
+      'title' => 'Post 5',
+      'author' => 'Michele Papagni',
+      'text' => 'Testo post 5'
+    ],
+    [
+      'title' => 'Post 6',
+      'author' => 'Michele Papagni',
+      'text' => 'Testo post 6'
+    ]
+  ],
+];
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -8,8 +59,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS v5.2.1 -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
 </head>
 
@@ -20,18 +70,31 @@
   <main>
 
 
-  
+    <?php
+
+    for ($i = 0; $i < count($posts); $i++) {
+      $thisArrayKey = array_keys($posts)[$i];
+      echo array_keys($posts)[$i] . '<br>';
+      for ($j = 0; $j < count($posts[$thisArrayKey]); $j++) {
+        $nomeArray = $j;
+        echo $posts[$thisArrayKey][$j]["title"] . '<br>';
+        echo $posts[$thisArrayKey][$j]["author"] . '<br>';
+        echo $posts[$thisArrayKey][$j]["text"] . '<br>' . "<hr>";
+      }
+    }
+
+    ?>
+
+
   </main>
   <footer>
     <!-- place footer here -->
   </footer>
   <!-- Bootstrap JavaScript Libraries -->
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
   </script>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
-    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
   </script>
 </body>
 
